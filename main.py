59c396bd6148bdbf2bb6bc5aedda1162f3d37a2e
JaeHyuk Jeong <75815463+mkonjkr@users.noreply.github.com>
2023-01-10 08:23:32 +0000

    added pico css and fake db

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
+#fake db
+db = {}
+
 @app.route("/") #decorator of a function right below
 def home():
   return render_template("home.html", name="nico")
@@ -12,9 +15,14 @@ def home():
 @app.route("/search")
 def search():
   keyword = request.args.get("keyword")
-  indeed = extract_indeed_jobs(keyword)
-  wwr = extract_wwr_jobs(keyword)
-  jobs = indeed + wwr
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    db[keyword] = jobs
+    
   return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("0.0.0.0")
