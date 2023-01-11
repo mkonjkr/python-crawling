cca48793478741532650017f77146f4a8c035f7c
JaeHyuk Jeong <75815463+mkonjkr@users.noreply.github.com>
2023-01-11 05:27:44 +0000

    added file export function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 #request goes to the web and requests for contents
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -15,6 +16,9 @@ def home():
 @app.route("/search")
 def search():
   keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  
   if keyword in db:
     jobs = db[keyword]
   else:
@@ -24,5 +28,15 @@ def search():
     db[keyword] = jobs
     
   return render_template("search.html", keyword=keyword, jobs=jobs)
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword") # get keyword from url
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run("0.0.0.0")
